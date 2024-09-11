716580fae42515b1c7940770bd2884a9a154cfe7
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2024-09-11 15:54:54 +0530

    dynamic navigation

--- router/src/app/page.tsx
+++ router/src/app/page.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
     <div >This is home page</div>
+    <Link href='/blog'>
+    <button>Blog</button>
+    </Link>
+    <br />
+    <br />
+    <br />
+    <Link href='/products'>
+    <button>Products</button>
+    </Link>
+    </>
   )
 }
 
--- router/src/app/products/page.tsx
+++ router/src/app/products/page.tsx
@@ -1,13 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        
+      <Link href="/">
+        <button>Go to Home</button>
+      </Link>
         <h1>product list</h1>
-        <p>product 1</p>
-        <p>product 2</p>
-        <p>product 3</p>
+        <p><Link href='products/1'>product 1</Link></p>
+        <p><Link href='products/2' >product 2</Link></p>
+        <p><Link href='products/3'>product 3</Link></p>
+       
     </div>
   )
 }
