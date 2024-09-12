5e6b51538506b7166af0d0d08fdbb45e7a308817
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2024-09-12 12:39:04 +0530

    layouts and templates

---
+++ router/src/app/(auth)/template.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+import { useState } from "react";
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const [input, setInput] = useState("");
+  return (
+    <div>
+      <div>
+        <input
+          className=" border-2 border-red-600"
+          value={input}
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={
+              isActive ? "font-bold text-blue-500 mr-4" : "text-black mr-4"
+            }
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
